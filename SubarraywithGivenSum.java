57b887360cd90290ac918ed0d061c7687a17d9b1
sudheerhadoop <sudhe9630@gmail.com>
2022-05-19 11:29:54 +0530

    Update SubarraywithGivenSum.java

--- SubarraywithGivenSum.java
+++ SubarraywithGivenSum.java
@@ -43,3 +43,29 @@ class Solution
     
     }
 }
+
+
+
+//another approach 
+
+private static int[] find(int[] a, int target) {
+  if (a.length < 2 || target < 0) {
+    return new int[]{-1,-1};
+  }
+  int start = 0, end = 1, n = a.length;
+  int runningSum = a[start] + a[end];
+  while (end < n) {
+    if (runningSum == target) {
+      return new int[]{start,end};
+    } else if (runningSum > target) {
+      runningSum -= a[start];
+      start++;
+    } else if (runningSum < target) {
+      end++;
+      if (end < n) {
+        runningSum += a[end];
+      }			
+    }
+  }
+  return new int[]{-1,-1};
+}
